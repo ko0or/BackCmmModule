403f509a6da53e8926a0193b078889ef967e60a5
minwoo <105141296+ko0or@users.noreply.github.com>
2024-05-03 18:42:34 +0900

    PageResponse.java 업데이트

--- src/main/java/back/cmm/module/cmm/base/util/PageResponse.java
+++ src/main/java/back/cmm/module/cmm/base/util/PageResponse.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Page;
 import java.util.List;
 import java.util.stream.Collectors;
 
-0@Getter
+@Getter
 public class PageResponse {
 
     private List<?> list;
