8e3ff0c2e067d7e3777ac8450cd725366f280f8c
ko0or <minu1871020679@gmail.com>
2024-10-15 00:00:59 +0900

    resource update (static dir)

--- src/main/resources/static/assets/SiteInfoPage-Dumw5TjA.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as n,o}from"./index-DTGUHq3f.js";const t={};function r(c,s){return o(),n("p",null," 사이트 정보 페이지 ")}const f=e(t,[["render",r],["__file","C:/Users/mw/pjts/frontend/editor/src/pages/info/SiteInfoPage.vue"]]);export{f as default};
