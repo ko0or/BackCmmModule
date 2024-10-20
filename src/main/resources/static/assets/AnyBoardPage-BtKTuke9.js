d5b474ee820e84bdeb584d5efae5ad971adba069
ko0or <minu1871020679@gmail.com>
2024-10-20 23:33:40 +0900

    리소스 업데이트

---
+++ src/main/resources/static/assets/AnyBoardPage-BtKTuke9.js
@@ -0,0 +1,1 @@
+/* empty css                                                                   */import{o as a,c as t,a as s}from"./index-CgG7z2Zf.js";const o={class:"section-area"},l={__name:"AnyBoardPage",setup(r){return(c,e)=>(a(),t("section",o,e[0]||(e[0]=[s("article",{class:"article-area"}," 자유 게시판 ",-1)])))}};export{l as default};
--- src/main/resources/static/assets/confirm-293RLkIB.js
+++
@@ -1,1 +0,0 @@
-import{J as r,K as i}from"./index-DZbwfADI.js";const s=(o,n)=>r.confirm(o,n,{confirmButtonText:"확인",cancelButtonText:"취소"}),f=()=>s("삭제 하시겠습니까?","삭제"),e={SUCCESS:"success",Warning:"warning",Info:"info",Error:"error"},c=(o,n,t)=>{i({title:o,message:n,type:t})},S=()=>{c("삭제 성공","선택한 내용이 삭제되었습니다",e.SUCCESS)};export{S as a,f as d};
