c8553a608b80396bbc9693d934c90cf3b33cf753
ko0or <minu1871020679@gmail.com>
2024-11-10 22:05:31 +0900

    리소스 업데이트

--- src/main/java/back/cmm/module/cmm/security/web/AuthApi.java
+++ src/main/java/back/cmm/module/cmm/security/web/AuthApi.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
